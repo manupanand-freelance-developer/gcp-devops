f5914a5b9a03128eebf4447a5c35b6972c43f883
manupanand <manupanandindianinvestor@gmail.com>
2025-08-04 12:05:07 +0530

    updated playbook scrip

--- terraform_codes/jenkins/ansible/run.sh
+++ terraform_codes/jenkins/ansible/run.sh
@@ -1,1 +1,1 @@
-	ansible-playbook -i localhost, -e ansible_user=$1 -e ansible_password=$2  -e role_name=jenkins-config playbook.yml
+	ansible-playbook -i localhost, -e ansible_user=$1 -e ansible_password=$2  -e role_name=jenkins-config playbook.yaml
