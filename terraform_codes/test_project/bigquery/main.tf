14f89f50f492cd076a5c6fb9922d37b129df883f
manupanand <manupanandindianinvestor@gmail.com>
2025-08-13 11:11:01 +0530

    update tble code

--- terraform_codes/test_project/bigquery/main.tf
+++ terraform_codes/test_project/bigquery/main.tf
@@ -8,3 +8,60 @@ resource "google_bigquery_dataset" "sensor_data" {
     env = "dev"
   }
 }
+
+resource "google_bigquery_table" "temperature_data" {
+  dataset_id = google_bigquery_dataset.default.dataset_id
+  table_id   = "temperature_data"
+
+  time_partitioning {
+    type = "DAY"
+  }
+
+  labels = {
+    env = "dev"
+  }
+
+  schema = <<EOF
+[
+  {
+    "name": "temperature",
+    "type": "FLOAT64",
+    "mode": "NULLABLE",
+    "description": "temperature in degree C"
+  },
+  {
+    "name": "sensor1",
+    "type": "FLOAT64",
+    "mode": "NULLABLE",
+    "description": "Sensor 1 data"
+  },
+  {
+    "name": "sensor2",
+    "type": "FLOAT64",
+    "mode": "NULLABLE",
+    "description": "Sensor 2 data"
+  },
+  {
+    "name": "sensor3",
+    "type": "FLOAT64",
+    "mode": "NULLABLE",
+    "description": "Sensor 3 data"
+  },
+  {
+    "name": "sensor4",
+    "type": "FLOAT64",
+    "mode": "NULLABLE",
+    "description": "Sensor 4 data"
+  },
+  {
+    "name": "sensor5",
+    "type": "FLOAT64",
+    "mode": "NULLABLE",
+    "description": "Sensor 5 data"
+  }
+  
+  
+]
+EOF
+
+}
