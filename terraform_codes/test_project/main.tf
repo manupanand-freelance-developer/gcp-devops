95b203b674e470faf0a47854c0628d6a6e88e8ff
manupanand <manupanandindianinvestor@gmail.com>
2025-08-13 10:23:33 +0530

    terraform bigquery

--- terraform_codes/test_project/main.tf
+++ terraform_codes/test_project/main.tf
@@ -1,7 +1,11 @@
-module "artifacroty_registry" {
-  source = "./artifactory_registry"
-  af_region= var.af_region
-}
-module "vps" {
-  source = "./vpc"
+# module "artifacroty_registry" {
+#   source = "./artifactory_registry"
+#   af_region= var.af_region
+# }
+# module "vps" {
+#   source = "./vpc"
+# }
+# 
+module "bigquery" {
+  source = "./bigquery"
 }
