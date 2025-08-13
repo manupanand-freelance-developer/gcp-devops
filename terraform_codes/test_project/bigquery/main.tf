5eb7d0b4d322fafeb89b14de33ad86a2f1b085c3
manupanand <manupanandindianinvestor@gmail.com>
2025-08-13 11:12:15 +0530

    test code

--- terraform_codes/test_project/bigquery/main.tf
+++ terraform_codes/test_project/bigquery/main.tf
@@ -10,7 +10,7 @@ resource "google_bigquery_dataset" "sensor_data" {
 }
 
 resource "google_bigquery_table" "temperature_data" {
-  dataset_id = google_bigquery_dataset.default.dataset_id
+  dataset_id = google_bigquery_dataset.sensor_data.dataset_id
   table_id   = "temperature_data"
 
   time_partitioning {
