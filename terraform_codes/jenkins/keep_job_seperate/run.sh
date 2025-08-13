35c82d10e2828110b3dea00a8f61d0140ed0cf88
manupanand <manupanandindianinvestor@gmail.com>
2025-08-13 21:06:41 +0530

    update code

--- terraform_codes/jenkins/keep_job_seperate/run.sh
+++ terraform_codes/jenkins/keep_job_seperate/run.sh
@@ -1,2 +1,2 @@
 export ANSIBLE_HOST_KEY_CHECKING=False
-ansible-playbook -i localhost, -e ansible_user=$1 -e ansible_password=$2  -e ansible_become_password=$2 -e role_name=jenkins-jobs create-jobs.yml -vv
+ansible-playbook -i localhost, -e ansible_user=$1 -e ansible_password=$2  -e ansible_become_password=$2 -e role_name=jenkins-jobs -e ip=$3 create-jobs.yml -vv
